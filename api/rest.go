a3cf2bbcf46437872dcae37521adb2d11e7c4781
amnonbb <amnonbb@gmail.com>
2026-01-28 13:40:06 +0200

    Make some props flexible

--- api/rest.go
+++ api/rest.go
@@ -1,37 +1,103 @@
 package api
 
 import (
+	"encoding/json"
 	"net/http"
+	"strconv"
 
 	"github.com/gin-gonic/gin"
 	log "github.com/sirupsen/logrus"
 )
 
+// FlexibleInt can unmarshal from both JSON number and JSON string
+type FlexibleInt int
+
+func (fi *FlexibleInt) UnmarshalJSON(data []byte) error {
+	// Try to unmarshal as number first
+	var num int
+	if err := json.Unmarshal(data, &num); err == nil {
+		*fi = FlexibleInt(num)
+		return nil
+	}
+
+	// If failed, try as string
+	var str string
+	if err := json.Unmarshal(data, &str); err != nil {
+		return err
+	}
+
+	// Convert string to int
+	if str == "" {
+		*fi = 0
+		return nil
+	}
+
+	num, err := strconv.Atoi(str)
+	if err != nil {
+		return err
+	}
+
+	*fi = FlexibleInt(num)
+	return nil
+}
+
+// FlexibleInt64 can unmarshal from both JSON number and JSON string
+type FlexibleInt64 int64
+
+func (fi *FlexibleInt64) UnmarshalJSON(data []byte) error {
+	// Try to unmarshal as number first
+	var num int64
+	if err := json.Unmarshal(data, &num); err == nil {
+		*fi = FlexibleInt64(num)
+		return nil
+	}
+
+	// If failed, try as string
+	var str string
+	if err := json.Unmarshal(data, &str); err != nil {
+		return err
+	}
+
+	// Convert string to int64
+	if str == "" {
+		*fi = 0
+		return nil
+	}
+
+	num, err := strconv.ParseInt(str, 10, 64)
+	if err != nil {
+		return err
+	}
+
+	*fi = FlexibleInt64(num)
+	return nil
+}
+
 type User struct {
-	Display    string   `json:"display"`
-	Email      string   `json:"email"`
-	Roles      []string `json:"roles"`
-	ID         string   `json:"id"`
-	Username   string   `json:"username"`
-	FamilyName string   `json:"familyname"`
-	Role       string   `json:"role"`
-	IsClient   bool     `json:"isClient"`
-	VHInfo     VHInfo   `json:"vhinfo"`
-	Allowed    bool     `json:"allowed"`
-	System     string   `json:"system"`
-	Extra      Extra    `json:"extra"`
-	Geo        Geo      `json:"geo"`
-	IP         string   `json:"ip"`
-	Country    string   `json:"country"`
-	Room       int      `json:"room"`
-	Janus      string   `json:"janus"`
-	Group      string   `json:"group"`
-	Camera     bool     `json:"camera"`
-	Question   bool     `json:"question"`
-	Timestamp  int64    `json:"timestamp"`
-	Session    int64    `json:"session"`
-	Handle     int64    `json:"handle"`
-	RFID       int64    `json:"rfid"`
+	Display    string        `json:"display"`
+	Email      string        `json:"email"`
+	Roles      []string      `json:"roles"`
+	ID         string        `json:"id"`
+	Username   string        `json:"username"`
+	FamilyName string        `json:"familyname"`
+	Role       string        `json:"role"`
+	IsClient   bool          `json:"isClient"`
+	VHInfo     VHInfo        `json:"vhinfo"`
+	Allowed    bool          `json:"allowed"`
+	System     string        `json:"system"`
+	Extra      Extra         `json:"extra"`
+	Geo        Geo           `json:"geo"`
+	IP         string        `json:"ip"`
+	Country    string        `json:"country"`
+	Room       FlexibleInt   `json:"room"`
+	Janus      string        `json:"janus"`
+	Group      string        `json:"group"`
+	Camera     bool          `json:"camera"`
+	Question   bool          `json:"question"`
+	Timestamp  int64         `json:"timestamp"`
+	Session    int64         `json:"session"`
+	Handle     int64         `json:"handle"`
+	RFID       FlexibleInt64 `json:"rfid"`
 }
 
 type VHInfo struct {
@@ -98,7 +164,8 @@ func getServerByID(c *gin.Context) {
 		"country_code": countryCode,
 		"city":         t.Geo.City,
 		"region":       t.Geo.Region,
-		"room":         t.Room,
+		"room":         int(t.Room),
+		"rfid":         int64(t.RFID),
 	}).Info("Client requesting server")
 
 	srv, err := getBestServerForCountry(countryCode)
@@ -113,8 +180,8 @@ func getServerByID(c *gin.Context) {
 	}
 
 	log.WithFields(log.Fields{
-		"username":     t.Username,
-		"country_code": countryCode,
+		"username":        t.Username,
+		"country_code":    countryCode,
 		"assigned_server": srv,
 	}).Info("Server assigned to client")
 
